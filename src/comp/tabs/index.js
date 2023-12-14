5eadda55bf47137fd94b7bdf232c5aca4df2d575
Mathieu <mathieu.leconte@bnpparibas.com>
2023-12-14 17:39:57 +0100

    test ba

--- src/comp/tabs/index.js
+++ src/comp/tabs/index.js
@@ -1,3 +1,9 @@
 import $ from "jquery";
 import "../../style.scss";
 import "./tabs.scss";
+const $tabLinks = document.querySelectorAll(".nav-link");
+$tabLinks.forEach( (link) => {
+    link.addEventListener('click', () => {
+        link.classList.remove("active");
+    })
+});
